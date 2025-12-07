c3357171c6188b3fde4a41995ed8c94e25656e60
Wyatt Queirolo <wfq2@cornell.edu>
2025-12-06 19:42:51 -0500

    updating to a second job

--- .github/workflows-src/test.ts
+++ .github/workflows-src/test.ts
@@ -7,8 +7,12 @@ export const testWorkflow = new Workflow("Test")
   .addJob("test", (job) =>
     job
       .runsOn("ubuntu-latest")
-      .addStep(setupAndInstallNode)
+      .addStep(...setupAndInstallNode)
       .addStep((step) => step.name("Run tests").run("npm run test"))
+  )
+  .addJob("test2", (job) =>
+    job
+      .runsOn("ubuntu-latest")
       .addStep((step) =>
         step.name("Run TypeScript function").runTypeScript(() => console.log("Hello, world!"))
       )
